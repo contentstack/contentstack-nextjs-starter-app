31d65dfb5294e2369b8018e53765d17209a91afc
Amitkanswal <amit.kanswal@contentstack.com>
2021-03-17 18:44:14 +0530

    restructured sdk-plugin functions

--- components/team-section.js
+++ components/team-section.js
@@ -9,19 +9,17 @@ export default function TeamSection(props) {
         {ourTeam.description ? <p>{ourTeam.description}</p> : ""}
       </div>
       <div className="team-content">
-        {ourTeam.employees?.map((employee, index) => {
-          return (
-            <div className="team-details" key={index}>
-              {employee.image && (
-                <img alt={employee.image.filename} src={employee.image.url} />
-              )}
-              <div className="team-details">
-                {employee.name && <h4>{employee.name}</h4>}
-                {employee.designation && <p>{employee.designation}</p>}
-              </div>
+        {ourTeam.employees?.map((employee, index) => (
+          <div className="team-details" key={index}>
+            {employee.image && (
+            <img alt={employee.image.filename} src={employee.image.url} />
+            )}
+            <div className="team-details">
+              {employee.name && <h4>{employee.name}</h4>}
+              {employee.designation && <p>{employee.designation}</p>}
             </div>
-          );
-        })}
+          </div>
+        ))}
       </div>
     </div>
   );
